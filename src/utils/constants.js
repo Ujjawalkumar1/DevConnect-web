b88fdb9822d0675d99e604d862eeefefb051d011
Ujjawal Kumar <ujjawalkumar787898@gmail.com>
2025-07-06 21:49:48 +0530

    deploy trying

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const BASE_URL = "https://devtinder-b5aj.onrender.com";    // for deploym
 
 //backend url render===>> (https://devtinder-b5aj.onrender.com)
 
-// export const BASE_URL = "http://localhost:3000";    // before deployment
+//export const BASE_URL = "http://localhost:3000";    // before deployment
 
 
 
