47373987f6bff45dc852670d201da4dfbd2da084
Ujjawal Kumar <ujjawalkumar787898@gmail.com>
2025-07-06 21:24:49 +0530

    for deployment - added backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,13 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://devtinder-b5aj.onrender.com";    // for deployment 
+
+//backend url render===>> (https://devtinder-b5aj.onrender.com)
+
+// export const BASE_URL = "http://localhost:3000";    // before deployment
+
+
+
+
+
 
 // export const BASE_URL = "http://localhost:7777";  for akshay saini backend urL.BUT FOR ME IT IS 3000
 
